868e83dc056518adf508c1fad666f4e3046fa251
Repnikov.A <repnikov.a@florcom.ru>
2023-08-08 12:25:52 +0300

    Добавьте файлы проекта.

---
+++ Modul_8/Task_8_1_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_8
+{
+    internal class Task_8_1_4
+    {
+        class DiscInfo
+        {
+            public string DiskName { get;}
+            public double DiskSpace { get; }
+            public double FreeDiscSpace { get; }
+            internal DiscInfo (string dname, double dspace, double fdspace)
+            {
+                DiskName = dname;
+                DiskSpace = dspace;
+                FreeDiscSpace = fdspace;
+            }
+
+        }
+    }
+}
